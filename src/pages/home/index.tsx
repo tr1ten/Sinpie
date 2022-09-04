94044bdfe2d65be5fc9ede50ed2b5b59a39f14e5
tr1ten <shubhi.blog@gmail.com>
2022-09-04 18:44:48 +0530

    fix: webpack not loading images

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,9 @@ import { AiFillFire } from "solid-icons/ai";
 import { createSlider, Slider, SliderButton, SliderProvider } from "solid-slider";
 import { DisplayProducts, Product } from "../../components/Sinpie/display-products";
 import './style.css';
+import Cascade1 from "../../../assets/cascade_1.jpg";
+import Cascade2 from "../../../assets/cascade_2.jpg";
+import Cascade3 from "../../../assets/cascade_3.jpg";
 import autoplay from "solid-slider/plugins/autoplay";
 
 const dummyProducts:Product[] = [
@@ -58,9 +61,9 @@ export const Home = () => {
     return (
         <main>
         <Slider options={{ loop: true }} plugins={[autoplay(3000, {})]}>
-            <img src="assets/cascade_1.jpg" alt='cascade-1' />
-            <img src="assets/cascade_2.jpg" alt='cascade-2' />
-              <img src="assets/cascade_3.jpg" alt='cascade-3' />
+            <img src={Cascade1} alt='cascade-1' />
+            <img src={Cascade2} alt='cascade-2' />
+              <img src={Cascade3} alt='cascade-3' />
 
         </Slider>
         <div class="hot-div">
